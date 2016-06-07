f4d2fdc421eefcb827fd0c68dd4ba295c64113ef
Dan Buda <danbuda@gmail.com>
2016-06-07 10:11:54 -0500

    Added notes & instructions

--- app/scripts/entry.js
+++ app/scripts/entry.js
@@ -1,14 +1,25 @@
-// import the stylesheet. this is necessary so that webpack will compile all the sass into css and then build it into our style.css file
+// Import the stylesheet. this is necessary so that webpack will compile all the sass into css and then build it into our style.css file
 import './../styles/main.scss';
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+// Notes:
+// Make different NavComponents and comment them out with instructions
+// as to what each one specifically does.
+// 1. Left-side hambugrer
+// 2. Right-side hamburger
+// 3. Slide-down for both left- and right-side hamburgers
+// 4. Slide in fron the left/right for left/right hamburgers
+// Add bit of template code for someone to add additional links,
+// but if too many, won't look good when breakpoints expand.
+
 
 const NavComponent = React.createClass({
 	render: function() {
 		return (
 			<nav>
 				<div className="navWide">
-					<a href="#">WideLink 1</a>
+					<a href="#">Wide Link 1</a>
 					<a href="#">Wide Link 2</a>
 					<a href="#">Wide Link 3</a>
 				</div>
